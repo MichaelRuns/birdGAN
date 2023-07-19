8c0534cb6652848ed900238f7f9100858588394d
Michael Vernau <102547230+MichaelRuns@users.noreply.github.com>
2023-07-18 23:09:03 -0700

    add another generationf format and load models nicely

---
+++ load.py
@@ -0,0 +1,17 @@
+import classes
+import torch
+import pytorch_lightning as pl
+import matplotlib.pyplot as plt
+
+def main():
+    model_state = torch.load('models/date_18_07_2023_epochs_500_birdGAN2_latent_dim100.pt')
+    hyperparams = model_state['hparams']
+    state_dict = model_state['state_dict']
+    model = classes.GAN(lr=hyperparams['lr'], latent_dim=hyperparams['latent_dim'])
+    model.load_state_dict(state_dict)
+    model.eval()
+    for i in range(5):
+        model.generate_sample(time_names=True)
+    
+if __name__ == '__main__':
+    main()
