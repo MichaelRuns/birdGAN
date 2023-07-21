ffa0112b273f9a85ee78d83288c2d9ef36724dc3
Michael Vernau <102547230+MichaelRuns@users.noreply.github.com>
2023-07-20 17:03:53 -0700

    fill in app.py skeleton

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,39 @@
-import FastAPI
+from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+import torch
+import torchvision.transforms as transforms
+from PIL import Image
+import io
+import classes
+
+app = FastAPI()
+
+# CORS
+origins = ["https://localhost", "https://localhost:3000"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+device = torch.device('cpu')
+model = None
+
+def load_model():
+    global model
+    model_state = torch.load('models/date_19_07_2023_epochs_200_birdGAN2_latent_dim100_LR0_0005.pt')
+    hyperparams = model_state['hparams']
+    state_dict = model_state['state_dict']
+    model = classes.GAN(lr=hyperparams['lr'], latent_dim=hyperparams['latent_dim'])
+    model.load_state_dict(state_dict)
+    model.eval()
+
+@app.on_event("startup")
+async def startup_event():
+    load_model()
+
+@app.post("/generate")
+async def generate_image():
+    #forward pass
+    pass
